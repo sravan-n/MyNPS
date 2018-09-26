d052e7f73e9c403ce720e9fb08fcc014391cbf5a
Savan Kumar <sravankumar.n92@outlook.com>
2018-09-26 16:19:25 -0400

    to reduce lines of code in HTML for loops iniated, JSON data need to be implemented

--- js/index.js
+++ js/index.js
@@ -12,3 +12,22 @@ setInterval(function(){
 	var time = addingZero(date.getHours())+":"+addingZero(date.getMinutes())+":"+addingZero(date.getSeconds());
 	document.getElementById('clock').innerHTML = time;
 }, 1000)
+
+/* var news_data=[ "Utah Temp is very hot. Please bring water", 
+			"Zion is very busy, please plan accordingly",
+			"Campsites for booking available only before 15 days",
+			"Heavy fire danger at Yosemite",
+			"Rockies now open" ];
+var news_text="";
+for (var i=0; i<news_data.length; i++) {
+	news_text+= '<li class="news">' + news_data[i] + '</li>';
+}
+document.getElementbyId("news_list").innerHTML = news_text;
+
+var link=[ "Mid Atlantic Region", "National Capital Region", "Southeast Region", "Midwest Region", "Southwest Region", 
+			"Rocky Moutain Region", "Western Region", "Western Region", "Pacific Northwest and Alaska Region" ];
+var link_text="";
+for (var i=0; i<link.length; i++) {
+	link_text+= '<li class="links"> <a class="links" href="#">' + link[i] + '</a> </li>';
+}
+document.getElementById("links_list").innerHTML = link_text; */ 
